daf5740ce802f3f6dcd65cf34f3e69e583db806c
unsatifsed27 <unsatifsederror@gmail.com>
2024-01-16 15:26:44 +0100

    sweet: overlay: added missing rro-overlay

--- device.mk
+++ device.mk
@@ -338,9 +338,11 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 # Overlay-RRO
 PRODUCT_PACKAGES += \
+    CarrierConfigOverlay \
     SweetinproSettingsProvider \
     SweetinproWifiOverlay \
-    SweetWifiOverlay
+    SweetWifiOverlay \
+    WifiOverlay
 
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
