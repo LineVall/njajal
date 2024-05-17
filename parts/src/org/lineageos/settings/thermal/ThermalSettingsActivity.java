1250775ad7ac8b566c39fe12369389eede04d5c5
unsatifsed27 <unsatifsederror@gmail.com>
2024-05-17 08:41:25 +0700

    sweet: fix parts build on QPR2
    Adapt from commit 602abf7
    
    sm6150-common: parts: Enable use_resource_processor for all sysui deps
    This will make the build faster
    
    Test: m checkbuild
    Bug: 295208392
    Signed-off-by: unsatifsed27 <unsatifsederror@gmail.com>
    Signed-off-by: basamaryan <basam.aryan@gmail.com>
    Co-Authored-By: Peter Kalauskas <peskal@google.com>

--- parts/src/org/lineageos/settings/thermal/ThermalSettingsActivity.java
+++ parts/src/org/lineageos/settings/thermal/ThermalSettingsActivity.java
@@ -20,7 +20,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.widget.R;
+
 
 public class ThermalSettingsActivity extends CollapsingToolbarBaseActivity {
 
@@ -29,8 +29,12 @@ public class ThermalSettingsActivity extends CollapsingToolbarBaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getFragmentManager().beginTransaction().replace(R.id.content_frame,
-                new ThermalSettingsFragment(), TAG_THERMAL).commit();
+        
+        getFragmentManager()
+                .beginTransaction()
+                .replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame,
+                new ThermalSettingsFragment(), TAG_THERMAL)
+                .commit();
     }
 
     @Override
